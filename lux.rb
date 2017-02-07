81381609d8ef775cdbede93adfff038a3d6f2784
softprops <d.tangren@gmail.com>
2017-02-07 00:23:49 -0500

    update lux

--- lux.rb
+++ lux.rb
@@ -1,8 +1,8 @@
 class Lux < Formula
   desc "a kubernetes log multiplexor"
   homepage "https://github.com/softprops/lux"
-  url "https://github.com/softprops/lux/releases/download/v0.0.1/lux-Darwin-x86_64.tar.gz"
-  sha256 "cec31f04c2b13642ad34060b1c02e2954a8b45ab6e76ceb0898f3b0ddd3c163c"
+  url "https://github.com/softprops/lux/releases/download/v0.1.1/lux-Darwin-x86_64.tar.gz"
+  sha256 "206a114cef0927f84288af4a8624e823788d4620d14b0148e79749f3eae0253c"
 
   bottle :unneeded
 
