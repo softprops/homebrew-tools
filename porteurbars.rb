444776d178ec15df019a07dc85466fc8eb546e2f
softprops <d.tangren@gmail.com>
2017-04-19 00:43:39 -0400

    new version

--- porteurbars.rb
+++ porteurbars.rb
@@ -1,9 +1,9 @@
 class Porteurbars < Formula
   desc "a tool for sharing portable git hosted project templates"
   homepage "https://github.com/softprops/porteurbars"
-  url "https://github.com/softprops/porteurbars/releases/download/v0.0.1/porteurbars-Darwin-x86_64.tar.gz"
-  sha256 "5b94a5abfe6dca548afc4139230bdac8897407e64de7806998c90e006d052bc4"
-  version "v0.0.1"
+  url "https://github.com/softprops/porteurbars/releases/download/v0.0.2/porteurbars-Darwin-x86_64.tar.gz"
+  sha256 "0264cbf6639daf1a16d53a7096fe555222f3b9b7c412d3204ff3679b71a87bf7"
+  version "v0.0.2"
 
   bottle :unneeded
 
