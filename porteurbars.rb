f874fecdc4a37569968c6df88f297456c5440212
softprops <d.tangren@gmail.com>
2017-10-09 00:52:27 -0400

    update porteurbars hash

--- porteurbars.rb
+++ porteurbars.rb
@@ -2,7 +2,7 @@ class Porteurbars < Formula
   desc "a tool for sharing portable git hosted project templates"
   homepage "https://github.com/softprops/porteurbars"
   url "https://github.com/softprops/porteurbars/releases/download/v0.1.5/porteurbars-Darwin-x86_64.tar.gz"
-  sha256 "9d04538e468225e54629048091f9b8b9d2f40bf89d53e6b0330a16ee2dc80b18"
+  sha256 "1c52ef20c784e20630e022de452bbcbe2b1380d08baa8085ba59072ff9a6889e"
   version "v0.1.5"
 
   bottle :unneeded
