43cbbdea0e9e2a9a5a254960585f0b6557c148a8
softprops <d.tangren@gmail.com>
2017-11-19 18:30:26 -0500

    update sha

--- git-codeowners.rb
+++ git-codeowners.rb
@@ -2,7 +2,7 @@ class GitCodeowners < Formula
   desc "a git extention for Github CODEOWNERS files"
   homepage "https://github.com/softprops/git-codeowners"
   url "https://github.com/softprops/git-codeowners/releases/download/v0.1.2/git-codeowners-Darwin-x86_64.tar.gz"
-  sha256 "03bc70d034c4e13d90988876166a0bc175c937896f0f9b5acefc7b5ab2d339ce"
+  sha256 "6b079461f78990dae9c338015784720e5c697563eefbdc4ded77955b97f646c6"
   version "v0.1.2"
 
   bottle :unneeded
