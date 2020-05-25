924ac11c5f5750f29f66f57e4f678c6891c041a0
softprops <d.tangren@gmail.com>
2020-05-25 16:21:08 -0400

    update octopat sha256

--- octopat.rb
+++ octopat.rb
@@ -2,7 +2,7 @@ class Octopat < Formula
   desc "An interactive GitHub personal access token command line dispenser ✨"
   homepage "https://github.com/softprops/octopat"
   url "https://github.com/softprops/octopat/releases/download/v0.0.1/octopat-Darwin-x86_64.tar.gz"
-  sha256 "0dff2ee3ced6c99198144bdb214277d14f99881e0bb26df4cb8970f564e3db54"
+  sha256 "fc954f2c90595299f9958c16556a811327986ed3fc33771639d3d4823ae12013"
   version "v0.1.5"
 
   bottle :unneeded
