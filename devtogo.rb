17eb95a08e28723b098daaa1516e5c7fb38679a2
softprops <d.tangren@gmail.com>
2020-06-15 01:01:02 -0400

    fix devtogo url

--- devtogo.rb
+++ devtogo.rb
@@ -1,7 +1,7 @@
 class Devtogo < Formula
     desc "👩🏽‍💻🎒 a dev.to tool for the road"
     homepage "https://github.com/softprops/devtogo"
-    url "https://github.com/softprops/octopat/releases/download/v0.0.1/devtogo-Darwin-x86_64.tar.gz"
+    url "https://github.com/softprops/devtogo/releases/download/v0.0.1/devtogo-Darwin-x86_64.tar.gz"
     sha256 "43a6ba151c412477a51b9f235c821cd02e8d578846d7c3779446e44740ba2612"
     version "v0.0.1"
   
