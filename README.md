a511fb865f36b6ea2f4d2086c2598ae9079bd694
softprops <d.tangren@gmail.com>
2020-06-15 00:59:53 -0400

    add devtogo

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Some Formule for installing some tools on osx
 
 ```
 # (optional) brew upgrade ...
+$ brew install softprops/tools/devtogo
 $ brew install softprops/tools/octopat
 $ brew install softprops/tools/porteurbars
 $ brew install softprops/tools/lux
---
+++ devtogo.rb
@@ -0,0 +1,18 @@
+class Devtogo < Formula
+    desc "👩🏽‍💻🎒 a dev.to tool for the road"
+    homepage "https://github.com/softprops/devtogo"
+    url "https://github.com/softprops/octopat/releases/download/v0.0.1/devtogo-Darwin-x86_64.tar.gz"
+    sha256 "43a6ba151c412477a51b9f235c821cd02e8d578846d7c3779446e44740ba2612"
+    version "v0.0.1"
+  
+    bottle :unneeded
+  
+    def install
+      bin.install "devtogo"
+    end
+  
+    test do
+      system "#{bin}/devtogo", "--help"
+    end
+  end
+  
