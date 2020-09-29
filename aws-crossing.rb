f5836e142d5cfbca5d89d3efb5b35909c5fa16a5
softprops <d.tangren@gmail.com>
2020-09-29 00:23:30 -0400

    update sha

--- aws-crossing.rb
+++ aws-crossing.rb
@@ -2,7 +2,7 @@ class AwsCrossing < Formula
     desc "🚸 Do cross AWS account work more efficiently"
     homepage "https://github.com/softprops/aws-crossing"
     url "https://github.com/softprops/aws-crossing/releases/download/v0.1.3/aws-crossing-Darwin-x86_64.tar.gz"
-    sha256 "f6068d899040159747751a6590610129b36ee27f3b4c0a42289b1fdc22abef43"
+    sha256 "429e92c620880d35e2f85842156dbcbb5fdcc1b943c008fa24a3efec53edcc91"
     version "v0.1.3"
   
     bottle :unneeded
